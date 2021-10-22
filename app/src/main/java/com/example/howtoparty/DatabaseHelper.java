007a59eaa798ad9e01b38e3b8f550afde94e2a7d
tobiashauzenberger <tobias@hauzenberger.name>
2021-10-22 17:28:06 +0200

    sqlite hinzugefügt und user tabelle eingerichtet Login hinzugefügt Registration hinzugefügt

---
+++ app/src/main/java/com/example/howtoparty/DatabaseHelper.java
@@ -0,0 +1,89 @@
+package com.example.howtoparty;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "howtoparty.db";
+    public static final int DB_VERSION = 1;
+
+    public static final String TABLE_NAME = "users";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_USERNAME = "username";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_VNAME = "vname";
+    public static final String COLUMN_NNAME = "nname";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_BDDATE = "bd_date";
+
+    public static final String SQL_CREATE_USERS =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_USERNAME + " TEXT NOT NULL, " +
+                    COLUMN_PASSWORD + " TEXT NOT NULL, " +
+                    COLUMN_VNAME + " TEXT NOT NULL, " +
+                    COLUMN_NNAME + " TEXT NOT NULL, " +
+                    COLUMN_EMAIL + " TEXT NOT NULL, " +
+                    COLUMN_BDDATE + " TEXT NOT NULL);";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_USERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    @SuppressLint("Recycle")
+    public boolean isLoginSuccessful (String userUsername, String passwort) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = null;
+
+        String sql = "SELECT * FROM users WHERE username = '" + userUsername +
+                "' AND password = '" + passwort + "';";
+        try {
+            data = db.rawQuery(sql, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data.getCount() == 1;
+    }
+
+    public void addUser (String username, String passwort, String vname, String nname, String email, String bddate) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_USERNAME, username);
+        values.put(COLUMN_PASSWORD, passwort);
+        values.put(COLUMN_VNAME, vname);
+        values.put(COLUMN_NNAME, nname);
+        values.put(COLUMN_EMAIL, email);
+        values.put(COLUMN_BDDATE, bddate);
+
+        long id = db.insert(TABLE_NAME, null, values);
+
+    }
+
+    public boolean validateUsername (String username) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String sql = "SELECT * FROM users WHERE username = '" + username + "';";
+
+        @SuppressLint("Recycle") Cursor data = db.rawQuery(sql, null);
+        return data.getCount() == 0;
+    }
+}
---
+++ app/src/main/java/com/example/howtoparty/RegisterActivity.java
@@ -0,0 +1,79 @@
+package com.example.howtoparty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    public DatabaseHelper db;
+    private Button btnRegister;
+    private EditText regUsername;
+    private EditText regVname;
+    private EditText regNname;
+    private EditText regEmail;
+    private EditText regBddate;
+    private EditText regPassword;
+    private EditText regPasswordWdh;
+    private TextView regErrorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        db = new DatabaseHelper(this);
+
+        btnRegister = (Button) findViewById(R.id.btnSaveRegister);
+        regUsername = (EditText) findViewById(R.id.regUsername);
+        regVname = (EditText) findViewById(R.id.regVname);
+        regNname = (EditText) findViewById(R.id.regNname);
+        regEmail = (EditText) findViewById(R.id.regEmail);
+        regBddate = (EditText) findViewById(R.id.regBddate);
+        regPassword = (EditText) findViewById(R.id.regPassword);
+        regPasswordWdh = (EditText) findViewById(R.id.regPasswordWdh);
+        regErrorText = (TextView) findViewById(R.id.regErrorText);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNewUser(
+                        regUsername.getText().toString(),
+                        regVname.getText().toString(),
+                        regNname.getText().toString(),
+                        regEmail.getText().toString(),
+                        regBddate.getText().toString(),
+                        regPassword.getText().toString(),
+                        regPasswordWdh.getText().toString()
+                );
+            }
+        });
+    }
+
+    private void saveNewUser(String username, String vname, String nname, String email, String dbdate, String password, String passwordWdh) {
+        boolean canBeSaved = true;
+
+        boolean validUsername = db.validateUsername(username);
+        if (!validUsername) {
+            canBeSaved = false;
+            regErrorText.setText("Der Username ist bereits vergeben");
+        }
+
+        if (!password.equals(passwordWdh)) {
+            canBeSaved = false;
+            regErrorText.setText("Das Passwort stimmt nicht überein");
+        }
+
+        if (canBeSaved) {
+            db.addUser(username, password, vname, nname, email, dbdate);
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+}
